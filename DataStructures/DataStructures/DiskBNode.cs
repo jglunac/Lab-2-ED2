62202267ffa2a86fcc415aee37bb43db088ce3d5
BrazilBatres <60472938+BrazilBatres@users.noreply.github.com>
2020-09-19 09:23:46 -0600

    Interfaz y Nodo

---
+++ DataStructures/DataStructures/DiskBNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    class DiskBNode<T> : IFixedLengthText where T : IFixedLengthText, IComparable
+    {
+        int ID;
+        int Dad;
+
+        int Grade;
+        T Value;
+       
+        int FixedLength => 8 + 4 * (Grade + 1) + Grade * Value.FixedLenght;
+        public string ToFixedLengthText()
+        {
+            return $"{ID:0000}{Dad:0000}";
+        }
+        
+    }
+}
